01ef0339723aead0cb32daf57f96d0cb6f020e1d
masaskrobo <masaskrobo@gmail.com>
2022-10-19 20:22:09 +0200

    addind useState Hook

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,9 +1,19 @@
 import './task.css'
+import React, {useState} from 'react'
 function Task({task, isRed}){
+    const [title, setTitle] = useState(task.title);
+
+    const clickHandler = () => {
+       setTitle('Updated');
+        console.log(title);
+    }
+
     return(
         <div className= {'task ' + (isRed ? 'red': ' ') } >
             <p>{task.text}</p>
-            <button>{task.isDone ? 'uradjeno' : 'nije uradjeno'}</button>
+            <h2>{title}</h2>
+            <button onClick={clickHandler}>{task.isDone ? 'uradjeno' : 'nije uradjeno'}</button>
+            
 
         </div>
     )
