961b0b1d53e4cb9cd251cb90aeaa47d5390cff20
masaskrobo <masaskrobo@gmail.com>
2022-10-31 14:26:10 +0100

    add profesor component

---
+++ src/components/profesor/profesor.js
@@ -0,0 +1,13 @@
+
+function Profesor({onProfesor}){
+    return(
+        <div>
+            <p>Profesori:</p>
+            <p>Ime:{onProfesor.ime}</p>
+            <p>Predmet:{onProfesor.predmet}</p>
+           
+
+        </div>
+    )
+}
+export default Profesor;
--- src/razred/razred.js
+++ src/razred/razred.js
@@ -1,28 +1,53 @@
 import Ucenik from  '../components/ucenik/ucenik.js';
+import Profesor from '../components/profesor/profesor.js'
 import './razred.css'
 function Razred(){
-    const ucenik1={
+
+    const profesori=[{
+            ime:'Ana Markovic',
+            predmet:'srpski',
+            
+        },
+        {
+            ime:'Stefan Dukic',
+            predmet:'fizicko',
+
+        },
+        {
+            ime:'Mila Jovanovic',
+            predmet:'muzicko',
+        }]
+   
+    const ucenici=[{
         ime:'Masa',
         godiste:'2002',
         predmet: 'srpski',
         vladanje:'5',
         razred:'2'
-
-
-    }
-    const ucenik2={
+    },
+    {
         ime:'Luka',
         godiste:'2000',
         predmet: 'fizicko',
         vladanje:'4',
         razred: '4'
+    }]
 
-    }
+    
     return(
         <div>
             <p className='razred'>Razred</p>
-            <Ucenik onUcenik={ucenik1}/>
-            <Ucenik onUcenik={ucenik2}/>
+
+            {profesori.map(function (item){
+                return <Profesor onProfesor={item} />
+            })}
+            
+           
+          {ucenici.map(function(item){
+                return <Ucenik onUcenik={item} />
+               })} 
+        
+           
         </div>
     )
 }
